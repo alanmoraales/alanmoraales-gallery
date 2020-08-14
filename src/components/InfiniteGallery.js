51821735cdcf8d80468c9fadaf3c145398c63f76
Alan Morales <alan_jesus99@hotmail.com>
2020-08-14 10:51:32 -0500

    added lightbox feat

--- src/components/InfiniteGallery.js
+++ src/components/InfiniteGallery.js
@@ -1,6 +1,8 @@
 import React, { useState, useCallback } from "react";
 import Gallery from "react-photo-gallery";
 import InfiniteScroll from "react-infinite-scroll-component";
+import Lightbox from "react-awesome-lightbox";
+import "react-awesome-lightbox/build/style.css";
 
 import GallerySpinner from "./GallerySpinner";
 import EndGalleryMessage from "./EndGalleryMessage";
@@ -12,7 +14,19 @@ const Wrapper = styled.div`
   margin: 0 auto;
 `;
 
-const InfiniteGallery = ({ thumbnails, next, hasMore }) => {
+const InfiniteGallery = ({ thumbnails, photos, next, hasMore }) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerOpen, setViewerOpen] = useState(false);
+
+  const openViewer = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerOpen(true);
+  }, []);
+
+  const closeViewer = () => {
+    setViewerOpen(false);
+  };
+
   return (
     <Wrapper>
       <InfiniteScroll
@@ -22,8 +36,17 @@ const InfiniteGallery = ({ thumbnails, next, hasMore }) => {
         loader={<GallerySpinner />}
         endMessage={<EndGalleryMessage />}
       >
-        <Gallery photos={thumbnails} margin={5} />
+        <Gallery photos={thumbnails} margin={5} onClick={openViewer} />
       </InfiniteScroll>
+      {viewerOpen ? (
+        <Lightbox
+          image={photos[currentImage].src}
+          title="photo by alanmoraales"
+          onClose={closeViewer}
+          allowRotate={false}
+          allowReset={false}
+        />
+      ) : null}
     </Wrapper>
   );
 };
